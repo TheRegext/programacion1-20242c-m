e048b262391c4a873615e3b2568d9fe397e9cef3
Brian Lara <brian.lara@outlook.com.ar>
2024-08-09 10:51:33 -0300

    Create ejemplosuma.cpp

---
+++ Semana 01/ejemplosuma.cpp
@@ -0,0 +1,28 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+
+using namespace std;
+
+///int integer
+int main(){
+    ///declaración de variables
+    int n1, n2, resta;
+    ///ingreso de datos
+    cout<<"BIENVENIDOS/AS AL PROGRAMA DE CALCULO DE SUMAS!!!!"<<endl<<endl;
+    cout<<"INGRESE UN NUMERO ";
+    cin>>n1;
+    cout<<"INGRESE UN NUMERO ";
+    cin>>n2;
+    ///cálculo de la suma
+    resta=n1+n2;
+    ///impresión en pantalla del resultado
+    cout<<"LA SUMA ES ";
+    cout<<resta;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
